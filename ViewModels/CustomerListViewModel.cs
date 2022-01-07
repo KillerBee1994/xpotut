8abc6d48fcd1a7e225adc7910035d5cc55bc9814
mohammad fauzi <hamode144@gmail.com>
2022-01-08 07:08:19 +0800

    Add files via upload

---
+++ ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,98 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Xpo;
+using System;
+using System.ComponentModel;
+using XpoTutorial;
+using System.Linq;
+using WpfApplicationMvvm.Services;
+using System.Windows.Input;
+
+namespace WpfApplicationMvvm.ViewModels {
+    public class CustomerListViewModel :ViewModelBase {
+
+
+        protected EditCustomerViewModel DialogViewModel { get; private set; }
+        public ICommand ShowMessageBoxCommand { get; private set; }
+        public ICommand ShowDialogCommand { get; private set; }
+
+        protected IDialogService DialogService { get { return GetService<IDialogService>(); } }
+        protected IMessageBoxService MessageService { get { return GetService<IMessageBoxService>(); } }
+
+        public CustomerListViewModel() {
+            ShowMessageBoxCommand = new DelegateCommand(OnShowMessageBoxCommandExecute);
+            ShowDialogCommand = new DelegateCommand(OnShowDialogCommandExecute);
+            DialogViewModel = new EditCustomerViewModel();
+        }
+        void OnShowMessageBoxCommandExecute() {
+            MessageService.Show("This is a message box", "DXMessageBoxService", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
+        }
+        void OnShowDialogCommandExecute() {
+            UICommand resCommand = DialogService.ShowDialog(DialogViewModel.DialogCommands, "DialogWindow", DialogViewModel);
+            if(resCommand != null) {
+                MessageService.Show("Dialog Clicked Button: " + resCommand.Caption);
+            }
+        }
+        public IListSource Customers {
+            get { return GetProperty(() => Customers);  }
+            private set { SetProperty(() => Customers, value); }
+        }
+        protected IDocumentManagerService DocumentManagerService {
+            get { return GetService<IDocumentManagerService>(); }
+        }
+        protected IInstantFeedbackService InstantFeedbackService {
+            get { return GetService<IInstantFeedbackService>(); }
+        }
+        protected override void OnInitializeInRuntime() {
+            base.OnInitializeInRuntime();
+            Reload();
+        }
+        protected void CreateDocument(int arg) {
+            IDocument doc = DocumentManagerService.FindDocument(arg, this);
+            if (doc == null) {
+                doc = DocumentManagerService.CreateDocument("EditCustomerView", arg, this);
+                doc.Id = string.Format("DocId_{0}", DocumentManagerService.Documents.Count());
+                doc.Title = string.Format("Edit Customer {0}", arg);
+            }
+            doc.Show();
+        }
+        [Command]
+        public void Reload() {
+            if (InstantFeedbackService == null)
+                ReloadCore();
+            else {
+                object focusedObjectKey = InstantFeedbackService.GetFocusedRowKey();
+                ReloadCore();
+                InstantFeedbackService.SetFocusedRowByKeyAsync(focusedObjectKey);
+            }
+        }
+        [Command]
+        public async void Delete() {
+            using (UnitOfWork uow = new UnitOfWork()) {
+                object key = InstantFeedbackService.GetFocusedRowKey();
+                Customer customer = uow.GetObjectByKey<Customer>(key);
+                uow.Delete(customer);
+                await uow.CommitChangesAsync();
+            }
+            Reload();
+        }
+        [Command]
+        public void Edit() {
+            object key = InstantFeedbackService.GetFocusedRowKey();
+            CreateDocument((int)key);
+        }
+        [Command]
+        public void Add() {
+            CreateDocument(-1);
+        }
+        private void ReloadCore() {
+            IDisposable old = (IDisposable)Customers;
+            Customers = new XPInstantFeedbackSource(typeof(Customer), "Oid;ContactName", null,
+                e => e.Session = new Session(),
+                e => e.Session.Session.Dispose());
+            if (old != null) {
+                old.Dispose();
+            }
+        }
+    }
+}
---
+++ ViewModels/EditCustomerViewModel.cs
@@ -0,0 +1,134 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB.Exceptions;
+using XpoTutorial;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WpfApplicationMvvm.ViewModels {
+    public class EditCustomerViewModel : ViewModelBase {
+        bool allowCloseDialog = false;
+        public bool AllowCloseDialog {
+            get { return allowCloseDialog; }
+            set { SetProperty(ref allowCloseDialog, value, () => AllowCloseDialog); }
+        }
+        string userName;
+        public string UserName {
+            get { return userName; }
+            set { SetProperty(ref userName, value, () => UserName); }
+        }
+        public List<UICommand> DialogCommands { get; private set; }
+        protected UICommand RegisterUICommand { get; private set; }
+        protected UICommand CancelUICommand { get; private set; }
+        public EditCustomerViewModel() {
+            if (!IsInDesignMode) {
+                UnitOfWork = new UnitOfWork();
+            }
+
+            DialogCommands = new List<UICommand>();
+            RegisterUICommand = new UICommand() {
+                Caption = "Register",
+                Command = new DelegateCommand<CancelEventArgs>(OnRegisterCommandExecute, OnRegisterCommandCanExecute),
+                IsDefault = true,
+                Id = MessageBoxResult.OK,
+            };
+            CancelUICommand = new UICommand() {
+                Caption = "Cancel",
+                Command = new DelegateCommand<CancelEventArgs>(OnCancelCommandExecute, OnCancelCommandCanExecute),
+                IsCancel = true,
+                Id = MessageBoxResult.Cancel,
+            };
+            DialogCommands.Add(RegisterUICommand);
+            DialogCommands.Add(CancelUICommand);
+        }
+        public Customer CurrentItem {
+            get { return GetProperty(() => CurrentItem); }
+            set { SetProperty(() => CurrentItem, value); }
+        }
+        public Order CurrentOrder {
+            get { return GetProperty(() => CurrentOrder); }
+            set { SetProperty(() => CurrentOrder, value); }
+        }
+        protected UnitOfWork UnitOfWork { get; private set; }
+        protected IMessageBoxService MessageBoxService {
+            get { return GetService<IMessageBoxService>(); }
+        }
+        protected IDocumentManagerService DocumentManagerService {
+            get { return GetService<IDocumentManagerService>(); }
+        }
+        protected override void OnParameterChanged(object parameter) {
+            if (UnitOfWork == null) return;
+            CurrentItem = UnitOfWork.GetObjectByKey<Customer>(parameter);
+            if (CurrentItem == null) {
+                CurrentItem = new Customer(UnitOfWork);
+            }
+        }
+        protected void CreateDocument(object arg) {
+            IDocument doc = DocumentManagerService.FindDocument(arg, this);
+            if (doc == null) {
+                doc = DocumentManagerService.CreateDocument("EditOrderView", arg, this);
+                doc.Id = string.Format("DocId_{0}", DocumentManagerService.Documents.Count());
+                Order order = arg as Order;
+                if (order == null)
+                    doc.Title = "Edit New Order";
+                else doc.Title = string.Format("Edit Order {0}", order.ProductName);
+            }
+            doc.Show();
+        }
+
+        void OnRegisterCommandExecute(CancelEventArgs parameter) {
+            if(!AllowCloseDialog) {
+                parameter.Cancel = true;
+            }
+        }
+        bool OnRegisterCommandCanExecute(CancelEventArgs parameter) {
+            return !string.IsNullOrEmpty(UserName);
+        }
+        void OnCancelCommandExecute(CancelEventArgs parameter) {
+            if(!AllowCloseDialog) {
+                parameter.Cancel = true;
+            }
+        }
+        bool OnCancelCommandCanExecute(CancelEventArgs parameter) {
+            return true;
+        }
+        [Command]
+        public async void Save() {
+            try {
+                await UnitOfWork.CommitChangesAsync();
+            } catch (LockingException) {
+                MessageBoxService.ShowMessage("The object was modified by another user. Please reload data.", "XPO Tutorial", MessageButton.OK, MessageIcon.Stop);
+                return;
+            }
+            ISupportParentViewModel spv = (ISupportParentViewModel)this;
+            CustomerListViewModel vm = (CustomerListViewModel)spv.ParentViewModel;
+            vm.Reload();
+            DocumentManagerService.ActiveDocument.Close();
+        }
+        [Command]
+        public void Cancel() {
+            DocumentManagerService.ActiveDocument.Close();
+        }
+        [Command]
+        public async void Reload() {
+            bool isNewObject = UnitOfWork.IsNewObject(CurrentItem);
+            UnitOfWork = new UnitOfWork();
+            CurrentItem = isNewObject ? new Customer(UnitOfWork) : await UnitOfWork.GetObjectByKeyAsync<Customer>(CurrentItem.Oid);
+        }
+        [Command]
+        public void EditOrder() {
+            CreateDocument(CurrentOrder);
+        }
+        [Command]
+        public void AddOrder() {
+            CreateDocument(CurrentItem);
+        }
+        [Command]
+        public void DeleteOrder() {
+            UnitOfWork.Delete(CurrentOrder);
+        }
+    }
+}
